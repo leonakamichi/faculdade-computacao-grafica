2aa7d1a705c5db6c77a4ead7f5881a99c583b07e
Leonardo Nakamichi <leonardo.hideak.nakamichi@gmail.com>
2021-05-20 16:49:42 -0300

    Adicionado textura de asfalto no ground

--- FINAL/js/trabalhoFinal.js
+++ FINAL/js/trabalhoFinal.js
@@ -1,265 +1,146 @@
 var scene; //mundo virtual
 var camera; //area de visualização
 var renderer; //responsavel por renderizar tudo
-var controls; //controle do mouser
+var controls; //controle do mouse
+//google-chrome --allow-file-access-from-files
+
 
 var parametrosGUI = {};
-var animationFolder;
 
 var elementos = [];
 
 var velocidade = 0.07;
 
+var ground;
 var geometriaA;
 
-var lights =[];
+var objLoading = function() {
+	loaderCarro = new THREE.OBJLoader();
+	loaderCarro.load(
+		'assets/carro/uploads_files_2027445_CyberpunkDeLorean.obj',
+		function(obj) {
+			
+			elementos['carro'] = obj;
 
-var wolfVelocity = 0;
+			let texLoader = new THREE.TextureLoader().setPath("assets/carro/textures/");
 
-//variaveis para animação
-var mixer;
-var modelReady = false;
-var animationActions = Array();
-var activeAction;
-var lastAction;
-var loadFinished;
+			obj.traverse(function(child) {
+					if(child instanceof THREE.Mesh) {
+						let material = new THREE.MeshStandardMaterial();
+						let materialBase = texLoader.load("DeLorean.jpg");
+						material.map = materialBase;
 
-var clock = new THREE.Clock();
-
-var objLoading = function(){
-	loaderFBX = new THREE.FBXLoader();
-
-	//Load da pista
-	loaderFBX.load(
-		'assets/pista/racetrackwithassets.fbx',
-		function(obj){
-			elementos['pista'] = obj;
-			let texLoader = new THREE.TextureLoader().setPath("assets/pista/");
-
-			obj.traverse( function (child){
-					if (child instanceof THREE.Mesh){
-                        child.material = new THREE.MeshLambertMaterial({
-							map: texLoader.load("DefaultMaterial_Base_Color.png"),
-                        });
-
-                        child.material.aoMap = texLoader.load("DefaultMaterial_Mixed_AO.png");
-                        child.material.normalMap = texLoader.load("DefaultMaterial_Normal_OpenGL.png");
-
-                        child.material.roughnessMap = texLoader.load("DefaultMaterial_Roughness.png");
-                        child.material.roughnessMap.wrapS = THREE.RepeatWrapping;
-
-                        child.material.metalnessMap = texLoader.load("DefaultMaterial_Metallic.png");
-                        child.material.metalnessMap.wrapS = THREE.RepeatWrapping;
-					}
+						child.material = material;
+					} 
 				}
 			);
 
-			obj.scale.y = 0.05;
-			obj.scale.z = 0.05;
-			obj.scale.x = 0.05;
+			obj.scale.y = 2; 
+			obj.scale.z = 2;
+			obj.scale.x = 2;
 
-			obj.position.y = -7.5;
+            obj.position.x = -16;
+			obj.position.y = -6;
+			obj.position.z = 300;
 
-			obj.rotation.y += 3.14;
+            obj.rotation.y += 0.9;
 
 			scene.add(obj);
-			console.log("Carregou Pista");
-			loadFinished = true;
+			console.log("Carregou Carro !");
 
-		});
-
-		//Load do carro 
-		loaderFBX.load(
-			'assets/carro/Models/car_1.fbx',
-			function(obj){
-				elementos['carro_policia'] = obj;
-				let texLoader = new THREE.TextureLoader().setPath("assets/carro/Textures/");
-	
-				obj.traverse( function (child){
-						if (child instanceof THREE.Mesh){
-							child.material = new THREE.MeshLambertMaterial({
-								map: texLoader.load("CarTexture1.png"),
-							});
-						}
-					}
-				);
-	
-				 obj.scale.y = 1;
-				 obj.scale.z = 1;
-				 obj.scale.x = 1;
-	
-				obj.position.y = -5.6;
-				obj.position.x = 18;
-				obj.position.z = 15;
-				
-				obj.rotation.x += 1.6;
-				obj.rotation.z += 1.6;
-				
-				scene.add(obj);
-				console.log("Carregou Carro 1");
-				loadFinished = true;
-	
-			});
+		},
+		function(andamento) {
+			console.log("Carregando..." + (andamento.loaded / andamento.total) * 100 + " %");
+		},
+		function(error) {
+			console.log("Deu merda!: "+ error);
+		},
+	);
 };
-//troca a ação do nosso modelo
-// const setAction = function(toAction) {
-//     if (toAction != activeAction) {
-//         lastAction = activeAction;
-//         activeAction = toAction;
-//         lastAction.stop();
-//         activeAction.reset();
-//         activeAction.play();
-//     }
-// }
 
-var ambientLightOn = function (){
-	lights['ambient'] = new THREE.AmbientLight(0xffffff,0.5);
-	scene.add(lights['ambient']);
-}
+var godSaysLightsOn = function() {
+	let spot = new THREE.SpotLight(0xffffff);
+	spot.position.set(100, 100, 100);
+	scene.add(spot);
 
-// var hemisphereLightOn = function(){
-// 	lights['hemisphere'] = new THREE.HemisphereLight(0xcce0ff, 0xffffff, 1);
-// 	scene.add(lights['hemisphere']);
-// }
-
-// var directionalLightOn = function () {
-// 	let light = new THREE.DirectionalLight(0xffffff,1);
-// 	light.castShadow = true;
-// 	light.shadow.mapSize.width = 4096;
-//     light.shadow.mapSize.height = 4096;
-//     light.shadow.camera.left = 1000;
-//     light.shadow.camera.bottom = 1000;
-//     light.shadow.camera.right = -1000
-//     light.shadow.camera.top = -1000;
-
-// 	light.position.y = 200;
-// 	light.position.x = 100;
-// 	light.target = elementos['pista'];
-
-
-// 	scene.add(light);
-// 	scene.add(light.target)
-
-// 	lights['directional'] = light;
-// }
-
-// var spotLightOn = function(){
-// 	let spot = new THREE.SpotLight(0xffffff, 0);
-// 	spot.angle = 0.3;
-// 	spot.castShadow = true;
-
-// 	spot.position.z = 40;
-// 	spot.position.y = 15;
-
-// 	spot.shadow.distance = 20;
-// 	spot.shadow.penumbra = 30;
-// 	spot.shadow.angle = 25;
-	
-// 	spot.target.position.set(0,5,0);
-
-// 	lights['spot'] = spot;
-// 	scene.add(spot);
-// }
-
-// var pointLightOn = function (){
-// 	let point = new THREE.PointLight(0xffffff, 3, 200);
-// 	lights['point'] = point;
-// 	point.castShadow = true;
-// 	point.position.y=10;
-// 	point.position.z = 10;
-
-// 	scene.add(point);
-// }
-
-var godSaysLightsOn = function (){
-	//hemisphereLightOn();
-	//directionalLightOn();
-	//spotLightOn();
-	//pointLightOn();
-	ambientLightOn();
-}
-
+	scene.add(new THREE.HemisphereLight( 0x443333, 0x222233, 4));
+};
 
 var createGui = function (){
 	const gui = new dat.GUI();
 
 	parametrosGUI = {
-		scalarPista: 1,
+		scalarPuppet: 1,
 		positionX: 0,
 		positionY: -6,
 		positionZ: 0,
-		ambientLight: 1,
-		sunLight: 1,
+		rotationY: 0,
 
-		skyColor : "#000000",
+		animalColor : "#000000",
+		groundColor: "#006400",
 
-		elementos: "",
+		animais: "",
+		modelGUI: ""
 	};
 
-	let fazScala = gui.add(parametrosGUI, 'scalarPista').min(0).max(0.10).step(0.01).name("Scale");
-	fazScala.onChange(function (parametro){
-			elementos[parametrosGUI.modelGui].scale.x = parametro;
-			elementos[parametrosGUI.modelGui].scale.y = parametro;
-			elementos[parametrosGUI.modelGui].scale.z = parametro;
-		}
-	);
-
-	let intensidadeLuz = gui.add(parametrosGUI, 'ambientLight').min(0).max(2).step(0.1).name("Ambient Light");
-	intensidadeLuz.onChange(function (parametro){
-			lights['ambient'].intensity = parametro;
-		}
-	);
-
-	let sunLight = gui.add(parametrosGUI, 'sunLight').min(0).max(2).step(0.1).name("Sun Light");
-	sunLight.onChange(function (parametro){
-			lights['directional'].intensity = parametro;
-		}
-	);
-
-
-	let opcoes = ['pista'];
-	let comboChange = gui.add(parametrosGUI, 'elementos').options(opcoes).name("Elementos");
+	let opcoes = ['Jaguar'];
+	let comboChange = gui.add(parametrosGUI, 'animais').options(opcoes).name("Animais");
 	comboChange.onChange(function(parametro){
-			if (parametro == 'pista'){
-				camera.lookAt(elementos["pista"].position);
-				parametrosGUI.modelGui = "pista";
+			if (parametro == 'Jaguar'){
+				camera.lookAt(elementos["jaguar"].position);
+				parametrosGUI.modelGUI = "jaguar";
 			}
 		}
 	);
+
 	let folderPosition = gui.addFolder("Position");
 
-	let positionX = folderPosition.add(parametrosGUI, 'positionX').min(0).max(600).step(15).name("Position X");
-	positionX.onChange(function (parametro){
-		lights['directional'].position.x = parametro;
+	let positionX = folderPosition.add(parametrosGUI, 'positionX').min(-30).max(30).step(0.1).name("Position X");
+	    positionX.onChange(function (parametro){
+		    elementos[parametrosGUI.modelGUI].position.x = parametro;
 		}
 	);
-	let positionY = folderPosition.add(parametrosGUI, 'positionY').min(0).max(600).step(15).name("Position Y");
-	positionY.onChange(function (parametro){
-		lights['directional'].position.y = parametro;
+	let positionY = folderPosition.add(parametrosGUI, 'positionY').min(-10).max(10).step(0.1).name("Position Y");
+	    positionY.onChange(function (parametro){
+			elementos[parametrosGUI.modelGUI].position.y = parametro;
 		}
 	);
-	let positionZ = folderPosition.add(parametrosGUI, 'positionZ').min(0).max(600).step(15).name("Position Z");
-	positionZ.onChange(function (parametro){
-			lights['directional'].position.z = parametro;
+	let positionZ = folderPosition.add(parametrosGUI, 'positionZ').min(-25).max(25).step(0.1).name("Position Z");
+	    positionZ.onChange(function (parametro){
+		    elementos[parametrosGUI.modelGUI].position.z = parametro;
 		}
 	);
+
+	let folderRotation = gui.addFolder("Rotation");
+
+	let rotationY = folderRotation.add(parametrosGUI, 'rotationY').min(-1).max(1).step(0.1).name("Rotation Y");
+		rotationY.onChange(function (parametro){
+			elementos[parametrosGUI.modelGUI].rotation.y += parametro;
+		}
+	);	
 
 	let colorFolder = gui.addFolder('Coloros');
-	let sColor = colorFolder.addColor(parametrosGUI, 'skyColor').name("SkyColor");
-	sColor.onChange(function (parametro){
-			scene.background= new THREE.Color(parametro);
+	let aColor = colorFolder.addColor(parametrosGUI, 'animalColor').name("Animal Color");
+	aColor.onChange(function (parametro){
+			elementos[parametrosGUI.modelGUI].traverse(function(child) {
+				if(child.isMesh) {
+						child.material.color = new THREE.Color(parametro);
+				} 
+			});
+	});
+
+	let gColor = colorFolder.addColor(parametrosGUI, 'groundColor').name("Ground");
+	    gColor.onChange(function (parametro){
+			ground.material.color.setHex(parametro.replace("#", "0x"));
 		}
 	);
 
-	animationFolder = gui.addFolder('Animations');
-    
 	gui.open();
-
 }
 
 var init = function (){
 	scene = new THREE.Scene();
-	scene.background = new THREE.Color(0xcce0ff);
+	scene.background = new THREE.Color(0xcce0ff)
 	
 	
 //	Camera em perspectiva
@@ -267,17 +148,18 @@ var init = function (){
 						50, // view angle
 						window.innerWidth/window.innerHeight, //aspect ratio
 						1, //near
-						500 //far
+						800 //far
 					);
 	
-	renderer = new THREE.WebGLRenderer({antialias: true});
-	renderer.shadowMap.enabled = true;
+	renderer = new THREE.WebGLRenderer();
 	renderer.setSize(window.innerWidth, window.innerHeight);
 	document.body.appendChild(renderer.domElement);
 		
-	camera.position.z = 63;
+	camera.position.z = 400;
 	camera.position.x = 0;
-	camera.position.y = 20;
+	camera.position.y = 50;
+
+	godSaysLightsOn();
 	
 	createGui();
 
@@ -285,23 +167,52 @@ var init = function (){
 
 	animation();
 
-	godSaysLightsOn();
+
+	//criar um piso.
+	let textureLoad = new THREE.TextureLoader();
+	let groundTexture = textureLoad.load("assets/asfalto/Road007_2K_Color.jpg"); //busca a imagem
+	groundTexture.wrapS = groundTexture.wrapT = THREE.RepeatWrapping; //quero que ela se repita
+	groundTexture.encoding = THREE.sRGBEncoding; //padrão cores, sempre que existir será informado
+	groundTexture.repeat.set(1, 1); //número de vezes que ela vai se repetir dentro do nosso chão
+
+	let materialGround = new THREE.MeshStandardMaterial({map: groundTexture});
+	materialGround.normalMap = textureLoad.load("assets/asfalto/Road007_2K_Normal.jpg"); //busca a normal, que dá noção de profundidade
+	materialGround.roughnessMap = textureLoad.load("assets/asfalto/Road007_2K_Roughness.jpg");
+	materialGround.displacementMap = textureLoad.load("assets/asfalto/Road007_2K_Displacement.jpg");
+	materialGround.displacementScale = 1;
+	materialGround.displacementBias = 1;
+
+	ground = new THREE.Mesh(
+		new THREE.PlaneBufferGeometry(100, 800),
+		materialGround
+	);
+
+	ground.rotation.x = - Math.PI/2;
+	ground.position.y-=7.5;
+	scene.add(ground);
 
 	controls = new THREE.OrbitControls(camera, renderer.domElement);
 
-	scene.fog = new THREE.Fog(0xcce0ff, 100, 500);
-
+	scene.fog =  new THREE.Fog(0xcce0ff, 400, 900);
 
 	document.addEventListener('keydown', apertouButao);
 	document.addEventListener('keyup', soltouBotao);
-
 };
 
+var key_r = false;
 var key_space = false;
+var key_q = false;
 
 var soltouBotao = function(e){
+
+	if (e.keyCode == 82){ //r
+		key_r = false;
+	}
 	if (e.keyCode == 32){ //espaço
 		key_space = false;
+	}
+	if (e.keyCode == 81){ //espaço
+		key_q = false;
 	}
 }
 
@@ -309,34 +220,28 @@ var soltouBotao = function(e){
 var apertouButao =  function(e){
 	console.log(e.keyCode);
 
+	if (e.keyCode == 82){ //r
+		key_r = true;
+	}
 	if (e.keyCode == 32){ // space
 		key_space = true;
 		pulando = true;
-		if (lights['spot'].intensity == 0) 
-			lights['spot'].intensity = 1;
-		else
-			lights['spot'].intensity = 0;
 	}
 
-	if (e.keyCode == 37){
-		elementos["carro_policia"].position.x -= 1.0;
+	if (e.keyCode == 81){ // q
+		key_q = true;		
 	}
 
-	if (e.keyCode == 39){
-		elementos["carro_policia"].position.x += 1.0;
+	if (e.keyCode == 38){ //douwn
+		camera.position.z-=0.5;
 	}
-
-	if (e.keyCode == 40){ //douwn
-		elementos["carro_policia"].position.z += 1.0;
-	}
-	if (e.keyCode == 38){ // UP
-		elementos["carro_policia"].position.z -= 1.0;
+	if (e.keyCode == 40){ // UP
+		camera.position.z+=0.5;
 	}
 }
 
 var animation = function (){
-	requestAnimationFrame(animation); 
-
+	requestAnimationFrame(animation); //adiciona o método na fila de renderização
 	
 	renderer.render(scene, camera); //tira uma foto do estado e mostra na tela
 }
